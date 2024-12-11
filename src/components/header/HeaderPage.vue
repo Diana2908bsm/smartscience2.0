6fca3017b18a332db7bf5684bf53e2635b4726ea
diana <diana.barantseva@gg.agency>
2024-12-11 17:44:20 +0300

    Правка бургер меню

--- src/components/header/HeaderPage.vue
+++ src/components/header/HeaderPage.vue
@@ -19,7 +19,7 @@
       </div>
     </div>
     <div class="burger-navbar">
-      <nav-bar v-if="isMenuOpen" @closeMenu="toggleMenu"></nav-bar>
+      <navbar v-if="isMenuOpen" @closeMenu="toggleMenu"></navbar>
     </div>
   </header>
 </template>
